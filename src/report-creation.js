e41c805be78fb5184d54920b35a1c908aabcf6ac
ariaguia <ariamx@gmail.com>
2023-01-25 19:30:53 +0100

    made minor changes to fillData() and buildChart().

--- src/report-creation.js
+++ src/report-creation.js
@@ -1,40 +1,40 @@
 /* exported buildChart */
 
-/** Fills a spreadsheet with a data matrix
- * @param {Object} spreadSheet - Spreadsheet to be filled with values.
+/** Fills a sheet with a data matrix
+ * @param {Object} sheet - sheet to be filled with values.
  * @param {Array<Array.<number>>} values - 2D array of values to be
  * inserted into the sheet.
  */
-function fillData(spreadSheet, values) {
+function fillData(sheet, values) {
   const numberRows = values.length;
   const numberCols = values[0].length;
   const range = `R1C1:R${numberRows}C${numberCols}`;
-  spreadSheet.getRange(range).setValues(values);
+  sheet.getRange(range).setValues(values);
 }
 
 /** Embeds a Chart into a given sheet
- * @param {Object} spreadSheet - Spreadsheet to get active sheet from.
+ * @param {Object} sheet - sheet to embed a chart into.
  * @param {Array<Array.<number>>} values - 2D array of values inside \
  * the sheet, to add as range to the chart builder.
- * @param {String} chartTitle - query name or spreadsheet title.
+ * @param {String} chartTitle - query name or sheet title.
  */
-function buildChart(spreadSheet, values, chartTitle) {
-  // Get sheets from spreadsheet.
-  const sheets = spreadSheet.getSheets()[0];
-  // Build chart for the sheets.
-  const chartBuilder = sheets.newChart()
+function buildChart(sheet, values, chartTitle) {
+  // Get sheets from sheet.
+  const chartSheet = sheet.getSheets()[0];
+  // Build chart for the sheet.
+  const chartBuilder = chartSheet.newChart()
     .setChartType(Charts.ChartType.BAR)
-    .addRange(sheets.getRange(2, 1, values.length, values[0].length))
+    .addRange(chartSheet.getRange(2, 1, values.length, values[0].length))
     .setPosition(2, 6, 0, 0)
-    .setOption('title', `${chartTitle.name}`)
+    .setOption('title', `${chartTitle}`)
     .setOption('titleTextStyle', {
       color: 'black',
-      fontSize: 18
+      fontSize: 18,
     } )
     .setOption('vAxis.title', `${values[0][0]}`)
     .setOption('hAxis.title', `${values[0][1]}`)
     .build();
 
-  // Insert chart into sheets.
-  sheets.insertChart(chartBuilder);
+  // Insert chart into sheet.
+  chartSheet.insertChart(chartBuilder);
 }
