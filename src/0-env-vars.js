1bea4d8962993afe4dc4d3df6619b2511c1d1e88
ariaguia <ariamx@gmail.com>
2023-02-07 20:58:55 +0100

    Deleted 0-env-vars.js file

--- src/0-env-vars.js
+++
@@ -1,7 +0,0 @@
-/** Get MB Session Token & Base URL from Script Environment.
- * Define @param token, @param url in > Project Settings > Script Properties
- */
-/* exported token, baseUrl */
-const scriptProperties = PropertiesService.getScriptProperties();
-const token = scriptProperties.getProperty('token');
-const baseUrl = scriptProperties.getProperty('url');
